480630100a1ed77c1452a97fb2a2a6743e94fca6
guoyinfeng <guoyinfeng@live.com>
2017-09-28 09:27:07 +0800

    init

---
+++ main.js
@@ -0,0 +1,32 @@
+(function (root) {
+  var ONE_SECOND = 1000;
+  var SECONDS = 60;
+
+  var ONE_MINUTE = ONE_SECOND * 60;
+  var MINUTES = 60;
+
+  var ONE_HOUR = ONE_MINUTE * 60;
+  var HOURS = 24;
+
+  var ONE_DAY = ONE_HOUR * 24;
+
+  var bornTime = new Date(2017, 3, 18, 17, 3).getTime()
+  root.setInterval(function () {
+    var duration = Date.now() - bornTime
+    console.log(new Date())
+
+    duration = duration / ONE_SECOND
+
+    var ss = Math.floor(duration % SECONDS)
+    duration = duration / SECONDS
+    var mm = Math.floor(duration % MINUTES)
+    duration = duration / MINUTES
+    var hh = Math.floor(duration % HOURS)
+    duration = duration / HOURS
+    var dd = Math.floor(duration % ONE_DAY)
+    console.log(dd, hh, mm, ss)
+
+    document.querySelector('div').innerHTML = dd + ' 天 ' + hh  + ' 小时 '+ mm  + ' 分钟 '+ ss + ' 秒 '
+
+  }, 1000);
+}(window))
